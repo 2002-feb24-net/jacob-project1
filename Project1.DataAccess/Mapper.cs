daebbd092821e51bb0b75edfc72a2ed0a5da6ded
Jacob Koch <jacob.koch.utd@gmail.com>
2020-04-01 18:21:23 -0500

    Setting up the data access and domain projects

---
+++ Project1.DataAccess/Mapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Project1.Domain.Model;
+namespace Project1.DataAccess
+{
+    public static class Mapper
+    {
+        //get the location entity
+        public static StoreLocation MapStoreLocationWithOrdersAndProduct(StoreLocation storeLocation)
+        {
+            return new StoreLocation
+            {
+                Id = storeLocation.Id,
+                LocationName = storeLocation.LocationName,
+                Orders = storeLocation.Orders.Select(Map).ToList(),
+                Product = storeLocation.Product.Select(Map).ToList()
+            };
+        }
+        //get the customer entity
+        public static Customer MapCustomerWithOrders(Customer customer)
+        {
+            return new Customer
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Orders = customer.Orders.Select(Map).ToList()
+            };
+        }
+        //get the orders entity
+        public static Orders Map(Orders order)
+        {
+            return new Orders
+            {
+                Id = order.Id,
+                StoreLocationId = order.StoreLocationId,
+                CustomerId = order.CustomerId,
+                ProductId = order.ProductId,
+                OrderTime = order.OrderTime,
+                Quantity = order.Quantity, 
+            };
+        }
+        //get the product entity
+        public static Product Map(Product product)
+        {
+            return new Product
+            {
+                Id = product.Id,
+                StoreLocationId = product.StoreLocationId,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock
+            };
+        }
+    }
+}
---
+++ Project1.Domain/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project1.Domain.Model
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
